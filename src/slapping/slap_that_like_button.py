ce9b152e8e10a5878e71f356ea42bbe06cf25a19
Violet <violet.hill@fresh4cast.com>
2022-10-08 13:46:48 +0100

    partial func coverage

--- src/slapping/slap_that_like_button.py
+++ src/slapping/slap_that_like_button.py
@@ -1,52 +1,56 @@
 """Main script."""
-# import enum
-
-# class LikeState(enum.Enum):
-#     """Status of a like button."""
-
-#     empty = enum.auto()
-#     liked = enum.auto()
-#     disliked = enum.auto()
+import enum
 
 
-# slap_like_transitions = {
-#     LikeState.empty: LikeState.liked,
-#     LikeState.liked: LikeState.empty,
-#     LikeState.disliked: LikeState.liked,
-# }
+class LikeState(enum.Enum):
+    """Status of a like button."""
 
-# slap_dislike_transitions = {
-#     LikeState.empty: LikeState.disliked,
-#     LikeState.liked: LikeState.disliked,
-#     LikeState.disliked: LikeState.empty,
-# }
+    empty = enum.auto()
+    liked = enum.auto()
+    disliked = enum.auto()
 
 
-# def slap_like(s: LikeState) -> LikeState:
-#     """Hit the like button."""
-#     return slap_like_transitions[s]
+slap_like_transitions = {
+    LikeState.empty: LikeState.liked,
+    LikeState.liked: LikeState.empty,
+    LikeState.disliked: LikeState.liked,
+}
+
+slap_dislike_transitions = {
+    LikeState.empty: LikeState.disliked,
+    LikeState.liked: LikeState.disliked,
+    LikeState.disliked: LikeState.empty,
+}
 
 
-# def slap_dislike(s: LikeState) -> LikeState:
-#     """Hit the dislike button."""
-#     return slap_dislike_transitions[s]
+def slap_like(s: LikeState) -> LikeState:
+    """Hit the like button."""
+    return slap_like_transitions[s]
 
 
-def add_two_number(x: int, y: int) -> int:
+def slap_dislike(s: LikeState) -> LikeState:
+    """Hit the dislike button."""
+    return slap_dislike_transitions[s]
+
+
+def add_two_number_extra(x: int, y: int) -> int:
     """Add two integers."""
-    x = int(x)
-    y = int(y)
-    return x + y
+    if x > 2:
+        x = int(x)
+        y = int(y)
+        return x + y
+    else:
+        return 0
 
 
-# def slap_many(s: LikeState, slaps: str) -> LikeState:
-#     """Hit many buttons in a row."""
-#     for c in slaps:
-#         c = c.lower()
-#         if c == "l":
-#             s = slap_like(s)
-#         elif c == "d":
-#             s = slap_dislike(s)
-#         else:
-#             raise ValueError("invalid slap")
-#     return s
+def slap_many(s: LikeState, slaps: str) -> LikeState:
+    """Hit many buttons in a row."""
+    for c in slaps:
+        c = c.lower()
+        if c == "l":
+            s = slap_like(s)
+        elif c == "d":
+            s = slap_dislike(s)
+        else:
+            raise ValueError("invalid slap")
+    return s
--- tests/test_slapping.py
+++ tests/test_slapping.py
@@ -1,56 +1,59 @@
 """Pytest tests."""
-# import pytest
+import pytest
 
-# from src.slapping.slap_that_like_button import LikeState, slap_many
-from src.slapping.slap_that_like_button import add_two_number
+from src.slapping.slap_that_like_button import (
+    LikeState,
+    add_two_number_extra,
+    slap_many,
+)
+
+
+def test_maths():
+    """Test for adding numbers."""
+    assert add_two_number_extra(3, 2) == 5, "Basic maths failed"
+
+
+def test_empty_slap():
+    """Test for empty slap."""
+    assert slap_many(LikeState.empty, "") is LikeState.empty
+
+
+def test_single_slap_liked():
+    """Test for like button slap."""
+    assert slap_many(LikeState.empty, "l") is LikeState.liked
 
 
 def test_single_slap_disliked():
-    """Test for adding numbers."""
-    assert add_two_number(1, 1) == 2, "Basic maths failed"
+    """Test for dislike button slap."""
+    assert slap_many(LikeState.empty, "d") is LikeState.disliked
 
 
-# def test_empty_slap():
-#     """Test for empty slap."""
-#     assert slap_many(LikeState.empty, "") is LikeState.empty
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("ll", LikeState.empty),
+        ("dd", LikeState.empty),
+        ("ld", LikeState.disliked),
+        ("dl", LikeState.liked),
+        ("ldd", LikeState.empty),
+        ("lldd", LikeState.empty),
+        ("ddl", LikeState.liked),
+    ],
+)
+def test_multi_slaps(test_input, expected):
+    assert slap_many(LikeState.empty, test_input) is expected
 
 
-# def test_single_slap_liked():
-#     """Test for like button slap."""
-#     assert slap_many(LikeState.empty, "l") is LikeState.liked
+@pytest.mark.skip(reason="regexes not supported yet")
+def test_regex_slaps():
+    assert slap_many(LikeState.empty, "[ld]*ddl") is LikeState.liked
 
 
-# def test_single_slap_disliked():
-#     """Test for dislike button slap."""
-#     assert slap_many(LikeState.empty, "d") is LikeState.disliked
+def test_invalid_slap():
+    with pytest.raises(ValueError):
+        slap_many(LikeState.empty, "x")
 
 
-# @pytest.mark.parametrize(
-#     "test_input,expected",
-#     [
-#         ("ll", LikeState.empty),
-#         ("dd", LikeState.empty),
-#         ("ld", LikeState.disliked),
-#         ("dl", LikeState.liked),
-#         ("ldd", LikeState.empty),
-#         ("lldd", LikeState.empty),
-#         ("ddl", LikeState.liked),
-#     ],
-# )
-# def test_multi_slaps(test_input, expected):
-#     assert slap_many(LikeState.empty, test_input) is expected
-
-
-# @pytest.mark.skip(reason="regexes not supported yet")
-# def test_regex_slaps():
-#     assert slap_many(LikeState.empty, "[ld]*ddl") is LikeState.liked
-
-
-# def test_invalid_slap():
-#     with pytest.raises(ValueError):
-#         slap_many(LikeState.empty, "x")
-
-
-# def test_print(capture_stdout):
-#     print("hello")
-#     assert capture_stdout["stdout"] == "hello\n"
+def test_print(capture_stdout):
+    print("hello")
+    assert capture_stdout["stdout"] == "hello\n"
